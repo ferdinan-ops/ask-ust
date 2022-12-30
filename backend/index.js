8480a7b47eb47fcaa9ac3f53f86efa2157517d0f
ferdinan-ops <perdinantumanggor95@gmail.com>
2022-12-30 16:07:40 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -23,10 +23,9 @@ dotenv.config();
 mongoose.set("strictQuery", false);
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: process.env.ORIGIN }));
+app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
