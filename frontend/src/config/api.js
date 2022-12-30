571641b3ed221b3cea02be3401c59e7467b6fb65
ferdinan-ops <perdinantumanggor95@gmail.com>
2022-12-30 15:38:52 +0700

    up

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-   baseURL: process.env.REACT_APP_API_URI,
+   baseURL: "https://ask-ust-api.vercel.app/api/v1",
    withCredentials: true,
    headers: { "Content-Type": "Application/json" },
 });
